9758318eddd68f3c385da6e87ca27437e5214860
Manuel Lorenzo <manulorenzop@gmail.com>
2022-09-22 10:54:08 +0200

    create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,36 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    user = User.find_by(session[:user_id])
+    params.require(:post).permit(:title, :body)
+
+    if user
+      @post.title = params[:post][:title]
+      @post.body = params[:post][:body]
+      @post.user_id = user.id
+      @post.save
+    else
+      flash[:notice] = "Unable to create post"
+      render :new, status: :unprocessable_entity
+    end
+
+    if @post
+      redirect_to post_path(@post.id)
+    else
+      flash[:notice] = "Unable to create post"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   resources :registrations, only: [:new, :create]
   resources :logins, only: [:new, :create]
-  # resources :posts
+  resources :posts
   root 'posts#index'
 end
