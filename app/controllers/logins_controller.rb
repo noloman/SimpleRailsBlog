15b185f86d04adea85344c056113eb9cac0a64de
Manuel Lorenzo <manulorenzop@gmail.com>
2022-09-22 19:51:02 +0200

    fix issue cause by not saving logged in user id

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,6 +6,7 @@ class LoginsController < ApplicationController
   def create
     @user = User.find_by(login_params)
     if @user
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       flash[:alert] = "Email or password is wrong"
